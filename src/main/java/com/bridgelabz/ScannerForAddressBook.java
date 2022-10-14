fa8ddc7ad5c2f4f719fca32286405ceba1bab484
Pavithrahc <pavithramubbu93@gmail.com>
2022-10-14 22:28:01 +0530

    welcome the AddressBookService Program

---
+++ src/main/java/com/bridgelabz/ScannerForAddressBook.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class ScannerForAddressBook {
+    public Scanner scannerProvider(){
+        Scanner scanner= new Scanner(System.in);
+        return  scanner;
+    }
+}
